503612050d65852ad77d14d1c5c5a9cc1761cd2d
ishtian_rev <ishtian11@gmail.com>
2022-04-02 21:40:45 +0600

    - hash calculation function added

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
+const SHA256 = require('crypto-js/sha256')
+
 class Block {
     constructor(index, timestamp, data, previousHash) {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
+        this.nonce = 0;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.timestamp + this.nonce + this.previousHash + JSON.stringify(this.data)).toString();
     }
 }
 
 class Blockchain {
-    
+
 }
